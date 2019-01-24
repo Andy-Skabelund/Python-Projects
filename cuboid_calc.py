d3763d630c86ce446748f69c849d0f1b484dc166
Andy-Skabelund <46804054+Andy-Skabelund@users.noreply.github.com>
2019-01-23 20:12:54 -0700

    Added Comments

--- cuboid_calc.py
+++ cuboid_calc.py
@@ -1,9 +1,15 @@
+# Andy Skabelund
+# Assignment 4
+# Calculates volume and surface area
 print("Welcome to the Cuboid Calculator!")
 print("Please enter values in feet.")
+# Assigns length variable
 length = float(input("Enter the length:"))
 print(length)
+# Assigns width variable
 width = float(input("Enter the width:"))
 print(width)
+# Assigns height variable
 height = float(input("Enter the height:"))
 print(height)
 print("Your", length, "X", width, "X", height, "cuboid has a Volume of", length * width * height, "cubic feet")
